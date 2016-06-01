5b5c37a405e219b869f43a5a59c621011083db84
varunteja <varunteja3634@gmail.com>
2016-06-01 22:49:16 +0530

    changed the app!

--- push_notifier/app.js
+++ push_notifier/app.js
@@ -1,15 +1,15 @@
 var myApp = angular.module("mainApp", []);
 
-myApp.controller('mainCtrl', function($scope) {
+myApp.controller('mainCtrl', function($scope, $interval) {
 
 
     $scope.notificationStatus = '';
 
-    $scope.notify = function () {
+    $scope.notify = function (title, theBody, theIcon ) {
 
       $scope.options = {
-        body: $scope.body,
-        icon: $scope.icon
+        body: theBody || $scope.body,
+        icon: theIcon || $scope.icon
       };
 
       if (!("Notification" in window)){
@@ -17,13 +17,13 @@ myApp.controller('mainCtrl', function($scope) {
 
       }else if(Notification.permission === 'granted'){
 
-        $scope.notification = new Notification($scope.title, $scope.options);
+        $scope.notification = new Notification(title || $scope.title, $scope.options);
         $scope.notificationStatus = 'granted';
 
       }else if(Notification.permission !== 'denied'){
           Notification.requestPermission(function (permission) {
             if(permission === 'granted'){
-              $scope.notification = new Notification($scope.title, $scope.options);
+              $scope.notification = new Notification( title || $scope.title, $scope.options);
               $scope.notificationStatus = 'granted';
 
             }
@@ -32,6 +32,19 @@ myApp.controller('mainCtrl', function($scope) {
         $scope.notificationStatus = 'denied';
       }
 
+      $scope.notification.onclick = function () {
+        window.open( theIcon || $scope.icon , 'notification_image');
+      };
+
     };
- 
+
+    $scope.runInterval = function () {
+
+      alert("you'll get notifications with random images! stay tuned!");
+      $interval(function () {
+        $scope.notify('Tring Tring!!', "Here's a random picture for you! with ❤️ from interval!", 'http://loremflickr.com/320/240/dog');
+      }, 10000, 50);
+      
+    };
+
 });
